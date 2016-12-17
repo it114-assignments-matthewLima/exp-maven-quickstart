4d3edc3b15a857dd5bb7098f222eb906d8ff49ff
Matt <Matt@Matt-PC.resource.campus.njit.edu>
2016-12-16 22:19:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#This is java project to experiment with maven_quickstart_archetype and hookup STS with github
+this is a line
+
+## Some subtitle
+Some explanation
+- a bullet
+- another bullet
---
+++ src/main/java/com/mycompany/exp_maven_quickstart/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.exp_maven_quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
